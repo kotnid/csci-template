2b4a93b16e4b124a9fd3c316ff160a3d1a43ec8a
kotnid <tkt0506tkt@gmail.com>
2026-01-12 13:54:08 +0800

    feat: queue
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

---
+++ queue.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+typedef struct queueCDT *queueADT;
+typedef int queueElementT;
+
+queueADT EmptyQueue(void);
+void Enqueue(queueADT queue, queueElementT element);
+queueElementT Dequeue(queueADT queue);
+int QueueLength(queueADT queue);
+int QueueIsEmpty(queueADT queue);
+
+
