1c38af3275b6bf3bb4abee70c26b334f26dbdb2c
Zyndesu <ericchansuiki@gmail.com>
2026-03-16 21:15:29 +0800

    fix: AVLTree

--- Abstract Data Type Implementation/AVLTreeMain.c
+++ Abstract Data Type Implementation/AVLTreeMain.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include "AVLTree.h"
 
-void printTreeADT(TreeNodeADT n) {
-    printf("DEBUG: n = %p\n", n);
-    if(n == NULL) printf("NULL\n");
-    printf("(%s, %d)\n", GetNodeKey(n), GetNodeData(n));
+void printCurrentNodeADT(AVLTreeADT t) {
+    // printf("DEBUG: n = %p\n", n);
+    if (t == NULL) {
+        printf("NULL\n");
+        return;
+    }
+    printf("(%s, %d)\n", GetNodeKey(AVLRoot(t)), GetNodeData(AVLRoot(t)));
 }
 
 void inOrder(AVLTreeADT t) {
-    if(t == NULL) return;
+    if (t == NULL) return;
     inOrder(t->lst);
-    printTreeADT(t->rt);
+    printCurrentNodeADT(t);
     inOrder(t->rst);
 }
-
 
 int main() {
     AVLTreeADT t = EmptyAVLTree();
@@ -22,33 +24,52 @@ int main() {
     TreeNodeADT nodeCharlie = NewTreeNode("Charlie", 2);
     TreeNodeADT nodeDave = NewTreeNode("Dave", 3);
     TreeNodeADT nodeEve = NewTreeNode("Eve", 4);
+    TreeNodeADT nodeFred = NewTreeNode("Fred", 5);
     t = AVLInsertNode(nodeAlice, t);
     t = AVLInsertNode(nodeBob, t);
     t = AVLInsertNode(nodeCharlie, t);
     t = AVLInsertNode(nodeDave, t);
     t = AVLInsertNode(nodeEve, t);
+    t = AVLInsertNode(nodeFred, t);
 
     printf("In-order Tree Tranversal\n");
     inOrder(t);
     printf("\n");
     /*
      * Expected Output:
-     * Alice: 0
-     * Bob: 1
-     * Charlie: 2
-     * Dave: 3
+     * (Alice, 0)
+     * (Bob, 1)
+     * (Charlie, 2)
+     * (Dave, 3)
+     * (Eve, 4)
+     * (Fred, 5)
      */
 
     printf("Root: ");
-    printTreeADT(AVLRoot(t));
+    printCurrentNodeADT(t);
     AVLTreeADT lChild = LeftAVLSubtree(t);
+    AVLTreeADT rChild = RightAVLSubtree(t);
     printf("Root->Left: ");
-    printTreeADT(AVLRoot(lChild));
+    printCurrentNodeADT(lChild);
     printf("Root->Right: ");
-    printTreeADT(AVLRoot((RightAVLSubtree(t))));
+    printCurrentNodeADT(rChild);
     printf("Root->Left->Left: ");
-    printTreeADT(AVLRoot(LeftAVLSubtree(lChild)));
+    printCurrentNodeADT(LeftAVLSubtree(lChild));
     printf("Root->Left->Right: ");
-    printTreeADT(AVLRoot(RightAVLSubtree(lChild)));
+    printCurrentNodeADT(RightAVLSubtree(lChild));
+    printf("Root->Right->Left: ");
+    printCurrentNodeADT(LeftAVLSubtree(rChild));
+    printf("Root->Right->Right: ");
+    printCurrentNodeADT(RightAVLSubtree(rChild));
     printf("\n");
+    /*
+     * Expected Output:
+     * Root: (Dave, 3)
+     * Root->Left: (Bob, 1)
+     * Root->Right: (Eve, 4)
+     * Root->Left->Left: (Alice, 0)
+     * Root->Left->Right: (Charlie, 2)
+     * Root->Right->Left: NULL
+     * Root->Right->Right: (Fred, 5)
+     */
 }
