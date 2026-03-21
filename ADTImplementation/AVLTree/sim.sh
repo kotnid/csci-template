88da1c1c19fc3a56819125f44d68438ae48408f0
Zyndesu <ericchansuiki@gmail.com>
2026-03-22 00:01:57 +0800

    feat(Tree Simulator): Tree traversal

--- ADTImplementation/AVLTree/sim.sh
+++
@@ -1,3 +0,0 @@
-#1/bin/bash
-gcc AVLSimulator.c "AVLTree(intNode).c" -o simulator -Wall -Wextra
-./simulator
---
+++ ADTImplementation/sim.sh
@@ -0,0 +1,3 @@
+#1/bin/bash
+gcc TreeSimulator.c "AVLTree/AVLTree(intNode).c" -o simulator -Wall -Wextra
+./simulator
