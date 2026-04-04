681ead34183c027f95947de44d5e30fa75b832aa
Zyndesu <ericchansuiki@gmail.com>
2026-04-04 20:12:53 +0800

    update: move tree simulator to independent repo

--- ADTImplementation/TreeSimulator/Simulator.h
+++
@@ -1,4 +0,0 @@
-#ifndef SIMULATOR_H_
-#define SIMULATOR_H_
-#include "Type.h"
-#endif // SIMULATOR_H_
--- ADTImplementation/TreeSimulator/sim.sh
+++
@@ -1,2 +0,0 @@
-#1/bin/bash
-gcc Simulator.c Util.c Commands.c ./ADTs/AVLTree.c ./ADTs/BinarySearchTree.c ./ADTs/TreeNode.c ./ADTs/SplayTree.c ./ADTs/RedBLackTree.c -o main -Wall -Wextra && ./main
